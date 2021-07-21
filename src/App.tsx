6d68c3afc50c2aedb11529895543d5813abbb88d
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-20 21:29:15 -0300

    feat: add react carrousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import StyleGlobal from './styles/global'
 import Routes from './routes'
 
 function App() {
 	return (
 		<BrowserRouter>
-		
 			<Routes />
 			<StyleGlobal />
 		</BrowserRouter>
