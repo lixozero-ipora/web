bd92550a02a3519aa81020635dfc8e0ce85e0996
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-22 23:27:29 -0300

    feat: create scroll to top

---
+++ src/hooks/useScrollTop.ts
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+
+const useScrollTop = (): void => {
+	useEffect(() => {
+		window.scroll({
+			top: 0,
+			behavior: 'smooth',
+		})
+	}, [])
+}
+
+export default useScrollTop
