5ec88b285c7d3131b2a62bdb4ed9f4cd956847a9
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-22 23:30:32 -0300

    feat: add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Landing from '../pages/Landing'
+import Schedule from '../pages/Schedule'
+import Complaint from '../pages/Complaint'
+import About from '../pages/About'
+import Admin from '../pages/Admin'
 
-function Routes() {
-	return (
-		<Switch>
-			<Route path="/" exact>
-				<Landing />
-			</Route>
-		</Switch>
-	)
-}
+const Routes: React.FC = () => (
+	<Switch>
+		<Route path="/" exact>
+			<Landing />
+		</Route>
+		<Route path="/horarios">
+			<Schedule />
+		</Route>
+		<Route path="/denuncia">
+			<Complaint />
+		</Route>
+		<Route path="/sobre">
+			<About />
+		</Route>
+		<Route path="/admin">
+			<Admin />
+		</Route>
+		<Route path="*">
+			<Redirect to="/" />
+		</Route>
+	</Switch>
+)
 
 export default Routes
