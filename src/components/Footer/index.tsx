2a8f8bef9cdbff60e6f501775a1824cd09c364a6
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-22 23:24:33 -0300

    feat: create page footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { BottomMessage, FooterContainer, FooterText } from './styles'
+
+const Footer: React.FC = () => (
+	<FooterContainer>
+		<FooterText>
+			<p>
+				Projeto desenvolvido pelos acadêmicos do curso de Análise e
+				Desenvolvimento de Sistemas do Instituto Federal Goiano Campus Iporá
+				durante turma da matéria de Gerência de Projetos ministrada pela
+				professora Luciana Recart Cardoso. O projeto foi feito juntamente com a
+				vereadora Viviane Specian, que colaborou como stakeholder.
+			</p>
+			<p>
+				Este é um projeto de código fonte aberto, para visualizar ou até mesmo
+				contribuir com o projeto acesse este{' '}
+				<a
+					href="https://github.com/lixozero-ipora/web"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					link
+				</a>
+				.
+			</p>
+			<p>
+				Todos os ícones utilizados não foram modificados.{' '}
+				<a
+					href="https://fontawesome.com/license"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Icones utilizados
+				</a>
+			</p>
+		</FooterText>
+		<BottomMessage>
+			Desenvolvido por{' '}
+			<a
+				href="https://www.ifgoiano.edu.br/home/index.php/cursos-superiores-ipora/221-analise-e-desenvolvimento-de-sistemas.html"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				TADS{' '}
+			</a>
+			&copy; 2021
+		</BottomMessage>
+	</FooterContainer>
+)
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+	margin-top: 50px;
+	width: 100%;
+`
+
+export const FooterText = styled.div`
+	padding: 10px 35px;
+
+	background-color: var(--color-green);
+	color: #fff;
+	font-size: 1.3rem;
+`
+
+export const BottomMessage = styled.p`
+	color: var(--color-text-dark);
+	padding: 0 35px;
+
+	a {
+		color: var(--color-blue-light);
+		font-weight: bold;
+	}
+`
