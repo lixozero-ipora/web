7f9b5226602e5e23389d06656b7c6f61799bd772
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-22 23:27:39 -0300

    feat: add global css colors

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,7 +2,12 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle` 
   :root {
+    font-size: 62.5%;
+
+    --color-red: #ef233c;
     --color-green: #28a443;
+    --color-text-dark: #4f4e4e;
+    --color-blue-light: #73D5FF;
   }
 
   *{
@@ -12,6 +17,7 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
   body{
+    font-size: 1.6rem;
     background: #ebebeb;
     font-family: Poppins, Arial, Helvetica, sans-serif;
   }
@@ -24,10 +30,25 @@ export default createGlobalStyle`
   } 
 
   a {
+    font-weight: bold;
+    color: inherit;
     text-decoration: none;
   }
 
   html, body, #root{
     min-height: 100%;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #166026;
+    border-radius: 5px;
   }
 `
