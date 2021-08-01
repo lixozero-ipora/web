f0e0f089e2c21e4ec45eca584a4db5e5627300d2
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 17:05:48 -0300

    feat: create editing schedule list

---
+++ src/components/EditingScheduleList/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { EditingSchedule } from '../../@types'
+import EditScheduleItem from '../EditScheduleItem'
+import { EditScheduleListContainer } from './styles'
+
+interface EditingScheduleListProps {
+	schedules: EditingSchedule
+	onClick(id: string): void
+}
+
+const EditingScheduleList: React.FC<EditingScheduleListProps> = ({
+	schedules,
+	onClick,
+}) => (
+	<EditScheduleListContainer>
+		{schedules.items.map((scheduleItem) => (
+			<EditScheduleItem
+				key={scheduleItem.id}
+				id={scheduleItem.id}
+				neighborhood={scheduleItem.neighborhood}
+				start={scheduleItem.start}
+				end={scheduleItem.end}
+				created_at={scheduleItem.created_at}
+				updated_at={scheduleItem.updated_at}
+				isActive={schedules.editing.includes(scheduleItem.id)}
+				onClickAction={onClick}
+			/>
+		))}
+	</EditScheduleListContainer>
+)
+
+export default EditingScheduleList
---
+++ src/components/EditingScheduleList/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const EditScheduleListContainer = styled.div``
