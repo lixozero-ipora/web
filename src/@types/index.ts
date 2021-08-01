daf14c5838b2dbe1aaeba80f134e810aee81bb15
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 20:31:19 -0300

    feat: add complaint type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -11,3 +11,21 @@ export interface EditingSchedule {
 	editing: string[]
 	items: ScheduleItem[]
 }
+
+interface Citizen {
+	name: string
+	adress: string
+	whatsapp: string
+	description: string
+	created_at: string
+}
+
+export interface Complaint {
+	id: string
+	latitude: number
+	longitude: number
+	occurrences: number
+	created_at: string
+	updated_at: string
+	citizens: Citizen[]
+}
