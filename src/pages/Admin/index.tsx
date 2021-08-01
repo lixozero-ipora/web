8c278a238871b023008d39ad0fb979a0758d64e1
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 17:08:03 -0300

    feat: create admin page

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,16 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedInputText from '../../components/AnimatedInputText'
 import { ButtonOutline } from '../../components/Common/styles'
 import NavBar from '../../components/Navbar'
+import { useAuth } from '../../store/authContext'
 import { AdminContainer } from './styles'
 
-const Admin: React.FC = () => (
-	<>
-		<NavBar />
-		<AdminContainer>
-			<ButtonOutline>Editar horários</ButtonOutline>
-			<ButtonOutline>Visualizar denúncias</ButtonOutline>
-		</AdminContainer>
-	</>
-)
+const Admin: React.FC = () => {
+	const { isLogedIn, handleLogin } = useAuth()
+	const [name, setName] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault()
+
+		handleLogin(name, password)
+	}
+
+	const handleChangeName = (input: string) => {
+		setName(input)
+	}
+	const handleChangeEmail = (input: string) => {
+		setPassword(input)
+	}
+
+	return (
+		<>
+			<NavBar />
+			<AdminContainer>
+				{isLogedIn ? (
+					<>
+						<ButtonOutline>
+							<Link to="/editar-horarios">Editar horários</Link>
+						</ButtonOutline>
+						<ButtonOutline>
+							<Link to="/visualizar-reclamacoes">Visualizar reclamações</Link>
+						</ButtonOutline>
+					</>
+				) : (
+					<form onSubmit={handleSubmit}>
+						<AnimatedInputText
+							label="Usuário"
+							value={name}
+							onChange={handleChangeName}
+						/>
+						<AnimatedInputText
+							label="Senha"
+							type="password"
+							value={password}
+							onChange={handleChangeEmail}
+						/>
+						<ButtonOutline type="submit">Enviar</ButtonOutline>
+					</form>
+				)}
+			</AdminContainer>
+		</>
+	)
+}
 
 export default Admin
--- src/pages/Admin/styles.ts
+++ src/pages/Admin/styles.ts
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
+import { ButtonOutline } from '../../components/Common/styles'
 
 export const AdminContainer = styled.div`
 	width: 90%;
+	height: 80vh;
 	margin: 20px auto;
 	display: flex;
 	align-items: center;
 	justify-content: space-evenly;
+
+	form {
+		width: 40%;
+
+		${ButtonOutline} {
+			display: block;
+			margin-top: 10px;
+			margin-left: auto;
+		}
+	}
 
 	@media (max-width: 992px) {
 		flex-direction: column;
