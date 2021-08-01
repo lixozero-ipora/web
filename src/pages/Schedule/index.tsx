f82719e0d1e78ab67c0a777058fea93414969bfa
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 17:07:24 -0300

    feat: change texts and connect to api

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { MdDateRange } from 'react-icons/md'
 
 import {
 	AuthorInfo,
-	BlurredImage,
+	Image,
 	BlurredImageContainer,
 	ContentContainer,
 	ContentText,
@@ -12,349 +13,86 @@ import {
 	Title,
 } from '../../components/Common/styles'
 import NavBar from '../../components/Navbar'
-import girlLikeImage from '../../assets/images/girl_like.png'
+import girlLikeImage from '../../assets/images/schedule_date.svg'
 import Footer from '../../components/Footer'
-import { ScheduleContainer } from './styles'
+import { ScheduleContainer, Table, TableHead, TableItem } from './styles'
 import useScrollTop from '../../hooks/useScrollTop'
+import useGetSchedules from '../../hooks/useGetSchedules'
+import Loading from '../../components/Loading'
 
 const Schedule: React.FC = () => {
+	const [schedules, isScheduleLoading] = useGetSchedules()
+
 	useScrollTop()
+
+	const updatedAt =
+		schedules.length > 1 ? new Date(schedules[0].updated_at) : null
 
 	return (
 		<>
 			<NavBar />
 			<BlurredImageContainer>
-				<BlurredImage src={girlLikeImage} />
+				<Image src={girlLikeImage} />
 			</BlurredImageContainer>
 			<ContentContainer>
-				<Title>Horários</Title>
+				<Title>
+					<span>
+						<MdDateRange size={60} />
+					</span>{' '}
+					Veja as datas de coleta
+				</Title>
 				<InfoContainer>
-					<AuthorInfo>Nome do autor</AuthorInfo>
-					<LastEdited>Última edição 19/07/2021</LastEdited>
+					<LastEdited>
+						Última edição{' '}
+						{updatedAt
+							? `${updatedAt.toLocaleString('pt-br', {
+									day: '2-digit',
+									month: '2-digit',
+									year: 'numeric',
+							  })}`
+							: null}
+					</LastEdited>
 				</InfoContainer>
 				<ContentText>
+					<p>A coleta de resíduos é feita da seguinte forma:</p>
+					<ul>
+						<li>
+							As rotas são definidas pela Secretaria de Obras da Prefeitura.
+						</li>
+						<li>
+							Os caminhões de coleta realizam a coleta, sob monitoramento via
+							GPS.
+						</li>
+						<li>
+							Os coletores recolhem os lixos nas portas das casas, coloca-os em
+							local estratégico e transfere para caminhões coletores, visando
+							agilidade e menor tempo na rota da coleta, de casa em casa. Fique
+							atento sobre a coleta em seu bairro e, caso seu lixo não seja
+							recolhido na porta de sua casa,{' '}
+							<Link to="/denuncia">reclame aqui</Link>.
+						</li>
+					</ul>
 					<p>
-						Aqui estão dispostos os horários sobre a coleta nos bairros, e
-						algumas informações de como são feitas essas coletas.
-					</p>
-					<p>
-						Os caminhões de lixo fazem a coleta de uma maneira planejada,
-						primeiro, pessoas catadoras coletam e armazenam estes lixos em um
-						local estratégico, para que o aproveitamento na rota da coleta seja
-						maior e, assim, seja possível coletar em mais bairros.
-					</p>
-					<p>
-						Os horários da coleta de cada bairro serão exibidos aqui. Fique
-						atento sobre a coleta em seu bairro e, caso não haja,{' '}
-						<Link to="/denuncia">denuncie</Link>.
-					</p>
-					<p>
-						Abaixo estão os bairros seguidos pelo horário definido para a
+						A seguir, constam os bairros conforme o horário definido para a
 						coleta.
 					</p>
 				</ContentText>
 				<ScheduleContainer>
-					<p>
-						Ariston Gomes <span className="green">21/07/2021</span> -
-						<span className="red">25/07/2021</span>
-					</p>
-					<p>
-						Bela Vista <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Central <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Centro <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Conjunto Águas Claras
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Conjunto Habitac Andorinhas
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Goiás <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Goiás II <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Arco Íris <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Arco-íris <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Monte Alto <span className="green">horário indefinido</span>{' '}
-						-<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Novo Horizont III
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Novo Horizonte
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Novo Horizonte I
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Novo Horizonte II
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Oliveiras <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Jardim Urânio <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Joaquim Berto <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Loteamento Perne <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Loteamento São Paulo Cruz
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Mato Grosso <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Mato Grosso do Sul <span className="green">horário indefinido</span>
-						- <span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Novo Horizonte <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Novo Ipor <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Padre Cícero <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Parque das Estrelas
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Parque Estrelas <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Pedro G Filho <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Perne <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Planalto <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Por do Sol <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Central <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor José <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Por do Sol <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Santo Antônio <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						São Francisco <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						São José <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						São Paulo da Cruz <span className="green">horário indefinido</span>{' '}
-						-<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Aeroporto Sul
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Ariston Gomes
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Bela Vista <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Carajás <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Central <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor dos Funcionário
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor dos Funcionários Ii
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Estrela Norte
-						<span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Funcionários <span className="green">horário indefinido</span>
-						- <span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Leste <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Perme <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Por do Sol <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Por Sol <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Setor Rosa Ventos <span className="green">horário indefinido</span>{' '}
-						-<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Sítio Perme <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Sossego <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Umuarama <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Brasília <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Brasília 1 <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Cascalheira <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Ferreira <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Iporazinho <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Itajubá <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Itajubá I <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Itajubá II <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Monte Alto <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Moreira <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Mutirão <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Nova <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Padre Cícero <span className="green">horário indefinido</span>{' '}
-						-<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Rica <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila Umuarama <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Vila União <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
-					<p>
-						Zona Rural <span className="green">horário indefinido</span> -
-						<span className="red">horário indefinido</span>
-					</p>
+					<Table>
+						<TableHead>
+							<p>Bairros</p>
+							<p>Início da coleta</p>
+							<p>Fim da coleta</p>
+						</TableHead>
+						{isScheduleLoading && <Loading />}
+						{schedules.map((schedule) => (
+							<TableItem data-aos="fade-left" key={schedule.id}>
+								<p>{schedule.neighborhood}</p>
+								<p>{schedule.start}</p>
+								<p>{schedule.end}</p>
+							</TableItem>
+						))}
+					</Table>
 				</ScheduleContainer>
 			</ContentContainer>
 			<Footer />
--- src/pages/Schedule/styles.ts
+++ src/pages/Schedule/styles.ts
@@ -9,36 +9,52 @@ export const InfoContainer = styled.div`
 export const ScheduleContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	margin: 0 auto;
+	padding: 10px;
+
+	background-color: white;
+	border-radius: 10px;
+`
+
+export const Table = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+export const TableItem = styled.div`
+	display: flex;
+	flex-wrap: nowrap;
+	color: var(--color-text-dark);
+	border-bottom: 1px solid var(--color-text-dark);
+
 	p {
 		display: flex;
 		justify-content: center;
+		text-align: start;
 		align-items: center;
+		word-wrap: break-word;
+	}
 
-		padding: 5px;
-		border-radius: 10px;
-		margin: 5px 0;
-		border: 1px solid var(--color-green);
-		background-color: #fff;
-
-		span {
-			display: inline-block;
-			margin-left: 5px;
-			padding: 3px;
-			color: #fff;
-			border-radius: 10px;
-		}
-		span.green {
-			background-color: var(--color-green);
-		}
-		span.red {
-			margin: 0;
-			background-color: var(--color-red);
-		}
+	p:nth-child(1) {
+		flex: 2;
+		justify-content: flex-start;
+	}
+	p:nth-child(2) {
+		flex: 1;
+		color: var(--color-green);
+	}
+	p:nth-child(3) {
+		flex: 1;
+		color: var(--color-red);
 	}
 
 	@media (max-width: 768px) {
-		p {
-			flex-direction: column;
+		p:nth-child(1) {
+			flex: 1;
 		}
 	}
+`
+
+export const TableHead = styled(TableItem)`
+	font-weight: bold;
+	border-width: 3px;
 `
