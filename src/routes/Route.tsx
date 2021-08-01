f83784717ce85a4271d7d31ec15ec496cc522629
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 17:08:29 -0300

    feat: create base route and use it

---
+++ src/routes/Route.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+	Redirect,
+	Route as RouteDOM,
+	RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom'
+import { useAuth } from '../store/authContext'
+
+interface RouteProps extends ReactDOMRouteProps {
+	isPrivate?: boolean
+	Component: React.ReactNode
+}
+
+const Route: React.FC<RouteProps> = ({ isPrivate, Component, ...rest }) => {
+	const { isLogedIn } = useAuth()
+
+	if (!isLogedIn && isPrivate) {
+		return <Redirect to="/" />
+	}
+
+	// eslint-disable-next-line react/jsx-props-no-spreading
+	return <RouteDOM {...rest}>{Component}</RouteDOM>
+}
+
+export default Route
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Redirect } from 'react-router-dom'
 
 import Landing from '../pages/Landing'
 import Schedule from '../pages/Schedule'
 import Complaint from '../pages/Complaint'
 import About from '../pages/About'
 import Admin from '../pages/Admin'
+import Route from './Route'
+import EditSchedule from '../pages/EditSchedule'
 
 const Routes: React.FC = () => (
 	<Switch>
-		<Route path="/" exact>
-			<Landing />
-		</Route>
-		<Route path="/horarios">
-			<Schedule />
-		</Route>
-		<Route path="/denuncia">
-			<Complaint />
-		</Route>
-		<Route path="/sobre">
-			<About />
-		</Route>
-		<Route path="/admin">
-			<Admin />
-		</Route>
-		<Route path="*">
-			<Redirect to="/" />
-		</Route>
+		<Route path="/" exact Component={<Landing />} />
+		<Route path="/datas" Component={<Schedule />} />
+		<Route path="/denuncia" Component={<Complaint />} />
+		<Route path="/sobre" Component={<About />} />
+		<Route path="/admin" Component={<Admin />} />
+		<Route path="/editar-horarios" Component={<EditSchedule />} isPrivate />
+		<Route path="/visualizar-denuncias" Component={<Admin />} isPrivate />
+
+		<Route path="*" Component={<Redirect to="/" />} />
 	</Switch>
 )
 
