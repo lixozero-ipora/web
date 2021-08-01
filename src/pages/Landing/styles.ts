18182fe0509110e5860aeaee88ec1bd66e73a27d
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 18:29:20 -0300

    feat: adapt to new logo

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -5,16 +5,12 @@ export const LadingContainer = styled.div``
 export const ImageContainer = styled.div`
 	display: flex;
 	justify-content: center;
-	margin: 20px;
-	padding: 15px;
-
-	background-color: white;
-	border-radius: 20px;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	margin-top: 20px;
 `
 
 export const LandingImage = styled.img`
-	width: 95%;
+	width: 100%;
+	object-fit: cover;
 `
 
 export const ContentContainer = styled.div`
