34f77aaf5da0922fc7effd03603619287945730a
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-07 12:54:40 -0300

    feat: make getSchedules a recursive function for more tries

--- src/hooks/useGetSchedules.ts
+++ src/hooks/useGetSchedules.ts
@@ -2,28 +2,34 @@ import { useEffect, useState } from 'react'
 import swal from 'sweetalert'
 import { ScheduleItem } from '../@types'
 import api from '../services/api'
+import sleep from '../utils/sleep'
 
 const useGetSchedules = (): [ScheduleItem[], boolean] => {
 	const [schedules, setSchedules] = useState<Array<ScheduleItem>>([])
 	const [loading, setIsLoading] = useState(true)
 
 	useEffect(() => {
-		getSchedules()
+		getSchedules(3)
 	}, [])
 
-	const getSchedules = async () => {
+	const getSchedules = async (retryCount: number, currentTry = 1) => {
 		try {
+			await sleep(3000)
 			const response = await api.get<Array<ScheduleItem>>('/schedules')
 
 			setSchedules(response.data)
-		} catch (error) {
-			swal(
-				'Ocorreu um erro',
-				'As nossas máquinas não estão funcionando no momento, por favor, tente mais tarde.',
-				'error'
-			)
-		} finally {
 			setIsLoading(false)
+		} catch (error) {
+			if (currentTry < retryCount) {
+				getSchedules(retryCount, currentTry + 1)
+			} else {
+				swal(
+					'Ocorreu um erro',
+					'As nossas máquinas não estão funcionando no momento, por favor, tente mais tarde.',
+					'error'
+				)
+				setIsLoading(false)
+			}
 		}
 	}
 
