3dc065a117c6c6e4fd715b4739363abb0b743c59
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-14 10:30:13 -0300

    feat: show loading in auth request

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -2,19 +2,24 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedInputText from '../../components/AnimatedInputText'
 import { ButtonOutline } from '../../components/Common/styles'
+import Loading from '../../components/Loading'
 import NavBar from '../../components/Navbar'
 import { useAuth } from '../../store/authContext'
 import { AdminContainer } from './styles'
 
 const Admin: React.FC = () => {
 	const { isLogedIn, handleLogin } = useAuth()
+	const [loading, setLoading] = useState(false)
 	const [name, setName] = useState('')
 	const [password, setPassword] = useState('')
 
 	const handleSubmit = (event: React.FormEvent) => {
 		event.preventDefault()
+		setLoading(true)
 
-		handleLogin(name, password)
+		handleLogin(name, password, () => {
+			setLoading(false)
+		})
 	}
 
 	const handleChangeName = (input: string) => {
@@ -28,6 +33,7 @@ const Admin: React.FC = () => {
 		<>
 			<NavBar />
 			<AdminContainer>
+				{loading && <Loading />}
 				{isLogedIn ? (
 					<>
 						<ButtonOutline>
@@ -38,20 +44,24 @@ const Admin: React.FC = () => {
 						</ButtonOutline>
 					</>
 				) : (
-					<form onSubmit={handleSubmit}>
-						<AnimatedInputText
-							label="Usuário"
-							value={name}
-							onChange={handleChangeName}
-						/>
-						<AnimatedInputText
-							label="Senha"
-							type="password"
-							value={password}
-							onChange={handleChangeEmail}
-						/>
-						<ButtonOutline type="submit">Enviar</ButtonOutline>
-					</form>
+					<>
+						{!loading && (
+							<form onSubmit={handleSubmit}>
+								<AnimatedInputText
+									label="Usuário"
+									value={name}
+									onChange={handleChangeName}
+								/>
+								<AnimatedInputText
+									label="Senha"
+									type="password"
+									value={password}
+									onChange={handleChangeEmail}
+								/>
+								<ButtonOutline type="submit">Enviar</ButtonOutline>
+							</form>
+						)}
+					</>
 				)}
 			</AdminContainer>
 		</>
