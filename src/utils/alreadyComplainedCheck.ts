6c3aa391fcae017348832421c102ccd37a3d4406
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-21 13:50:19 -0300

    refactor: change function type

--- src/utils/alreadyComplainedCheck.ts
+++ src/utils/alreadyComplainedCheck.ts
@@ -1,6 +1,6 @@
 import swal from 'sweetalert'
 
-const alreadyComplainedCheck = (): void => {
+const alreadyComplainedCheck = async (): Promise<boolean> => {
 	const complainedKey = localStorage.getItem('@complained')
 	if (complainedKey) {
 		const complained = JSON.parse(complainedKey)
@@ -8,13 +8,16 @@ const alreadyComplainedCheck = (): void => {
 		const complainedDate = new Date(complained.date)
 		// Day that the person complained + 3 days
 		if (complainedDate.getTime() + 60 * 3600 * 72 > Date.now()) {
-			swal(
+			await swal(
 				'Parece que você já reclamou',
 				'Tente novamente após três dias da última reclamação.',
 				'error'
 			)
+
+			return true
 		}
 	}
+	return false
 }
 
 export default alreadyComplainedCheck
